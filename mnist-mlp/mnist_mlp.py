9380443cbd2736d4456e769ecd226a2e383d453b
Farshid Delgosha <fardelgosha1@gmail.com>
2026-03-11 23:47:34 -0400

    Separated train and eval

--- mnist-mlp/mnist_mlp.py
+++ mnist-mlp/mnist_mlp.py
@@ -1,18 +1,55 @@
-from typing import Final
+import sys
+import copy
 import time
+from itertools import pairwise
+from typing import Tuple
 import torch
 from torch import Tensor
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, replace
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
 
 plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.sans-serif"] = ["Arial"]
+
+
+@dataclass(frozen=True)
+class MLPConfig:
+    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    layers_sizes: Tuple[int, ...] = (28 * 28, 128, 10)
+    activation: nn.Module = nn.ReLU()
+    transform: transforms.ToTensor = transforms.ToTensor()
+    data_storage_path: str = "./data"
+    batch_size: int = 64
+    learning_rate: float = 1e-3
+    epochs: int = 1
+    model_log_step: int = 50
+
+
+class MLP(nn.Module):
+    def __init__(self, mlp_config: MLPConfig) -> None:
+        super().__init__()
+
+        if len(mlp_config.layers_sizes) < 2:
+            sys.exit("ERROR: An MLP must have at least two layers!")
+
+        layers = []
+        for in_f, out_f in pairwise(mlp_config.layers_sizes):
+            layers.append(nn.Linear(in_f, out_f))
+            layers.append(copy.deepcopy(mlp_config.activation))
+
+        layers.pop()
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.flatten(x, 1)
+        return self.model(x)
 
 
 @dataclass
@@ -35,7 +72,8 @@ class MetricTracker:
 
         eps = 1e-8  # Used to avoid division by zero
         avg_grad = grads.mean().item()
-        avg_norm_ratio = (grads / (params + eps)).mean().item()
+        # avg_norm_ratio = (grads / (params + eps)).mean().item()
+        avg_norm_ratio = (grads.sum() / params.sum()).item()
 
         self.grad_history.append(avg_grad)
         self.param_history.append(params.mean().item())
@@ -45,81 +83,34 @@ class MetricTracker:
         self._param_buffer.clear()
 
 
-class MLP(nn.Module):
-    def __init__(self, hidden_layer_size: int) -> None:
-        super().__init__()
-        # Fully connected layer 1
-        self.fc1 = nn.Linear(28 * 28, hidden_layer_size)
-        # Rectified linear unit
-        self.relu = nn.ReLU()
-        # Fully connected layer 2
-        self.fc2 = nn.Linear(hidden_layer_size, 10)
+class MLPTrainer:
+    def __init__(self, mlp_config: MLPConfig, mlp: MLP):
+        self.config = mlp_config
 
-    def forward(self, x) -> torch.Tensor:
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        return x
-
-
-class DigitClassifier:
-    _DATA_STORAGE_PATH: Final[str] = "./data"
-    _MODEL_LOG_STEP = 50
-
-    def __init__(
-        self,
-        hidden_layer_size: int,
-        train_batch_size: int = 64,
-        learning_rate: float = 1e-3,
-        epoch: int = 1,
-    ) -> None:
-        self.device = torch.device("cpu")
-        self.train_batch_size = train_batch_size
-        self.epoch = epoch
-
-        transform = transforms.ToTensor()
         train_dataset = datasets.MNIST(
-            root=self._DATA_STORAGE_PATH, train=True, download=True, transform=transform
-        )
-        test_dataset = datasets.MNIST(
-            root=self._DATA_STORAGE_PATH,
-            train=False,
+            root=self.config.data_storage_path,
+            train=True,
             download=True,
-            transform=transform,
+            transform=self.config.transform,
         )
 
         self.train_loader = DataLoader(
-            train_dataset, batch_size=self.train_batch_size, shuffle=True
+            train_dataset, batch_size=self.config.batch_size, shuffle=True
         )
-        self.test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False)
-
-        self.hidden_layer_size = hidden_layer_size
-        self.model = MLP(hidden_layer_size).to(self.device)
+        self.model = mlp.to(self.config.device)
 
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        self.optimizer = optim.Adam(
+            self.model.parameters(), lr=self.config.learning_rate
+        )
 
         self.metrics = defaultdict(MetricTracker)
 
-        if learning_rate is not None:
-            for g in self.optimizer.param_groups:
-                g["lr"] = learning_rate
-
-        self.lr = self.optimizer.param_groups[0]["lr"]
-
-        print(
-            f"Hidden layer size: {self.hidden_layer_size}, train batch size: {self.train_batch_size}, learning rate: {self.lr}, epoch: {epoch}, training dataset size: {len(train_dataset)}, test dataset size: {len(test_dataset)}"
-        )
-
-    def get_logits(self, images) -> torch.Tensor:
-        return self.model(images.to(self.device))
-
     def get_loss(self, logits, labels) -> torch.Tensor:
-        return self.criterion(logits, labels.to(self.device))
+        return self.criterion(logits, labels.to(self.config.device))
 
     def log_metrics(self, sample_index: int) -> None:
-        is_log_step = ((sample_index + 1) % self._MODEL_LOG_STEP == 0) or (
+        is_log_step = ((sample_index + 1) % self.config.model_log_step == 0) or (
             sample_index + 1 == len(self.train_loader)
         )
 
@@ -133,27 +124,53 @@ class DigitClassifier:
             if is_log_step:
                 metrics.flush_to_history()
 
-    def train(self) -> None:
-        for _ in range(self.epoch):
+    def train(self) -> dict[str, MetricTracker]:
+        self.model.train()
+        for _ in range(self.config.epochs):
             for i, (images, labels) in enumerate(self.train_loader):
                 self.optimizer.zero_grad()
-                logits = self.get_logits(images)
+                logits = self.model(images.to(self.config.device))
                 loss = self.get_loss(logits, labels)
                 loss.backward()
                 self.optimizer.step()
                 self.log_metrics(i)
 
-    def evaluate(self) -> None:
+        return self.metrics
+
+
+class MLPEvaluator:
+    def __init__(self, mlp_config: MLPConfig, mlp: MLP):
+        self.config = mlp_config
+
+        test_dataset = datasets.MNIST(
+            root=self.config.data_storage_path,
+            train=False,
+            download=True,
+            transform=self.config.transform,
+        )
+
+        self.test_loader = DataLoader(
+            test_dataset, batch_size=self.config.batch_size, shuffle=False
+        )
+
+        self.model = mlp.to(self.config.device)
+
+    def evaluate(self) -> float:
         self.model.eval()
-        number_of_test_samples = len(self.test_loader.dataset)
         correct = 0
         with torch.no_grad():
             for images, labels in self.test_loader:
-                logits = self.get_logits(images)
+                logits = self.model(images.to(self.config.device))
                 pred = logits.argmax(dim=1)
                 correct += (pred == labels).sum().item()
 
-        self.accuracy = 100 * correct / number_of_test_samples
+        return 100 * correct / len(self.test_loader.dataset)
+
+
+class MLPPlotter:
+    def __init__(self, metrics: dict[str, MetricTracker], mlp_config: MLPConfig):
+        self.metrics = metrics
+        self.config = mlp_config
 
     def plot_metrics(self) -> None:
         fig1 = plt.figure("Gradients", figsize=(10, 5))
@@ -162,7 +179,8 @@ class DigitClassifier:
 
         for name, metrics in self.metrics.items():
             steps = [
-                (i + 1) * self._MODEL_LOG_STEP for i in range(len(metrics.grad_history))
+                (i + 1) * self.config.model_log_step * self.config.batch_size
+                for i in range(len(metrics.grad_history))
             ]
 
             for fig_num, metric in [
@@ -187,20 +205,22 @@ class DigitClassifier:
 
 
 def main() -> None:
-    hidden_layer_size = 128
-    train_batch_size = 64
-    learning_rate = 1e-3
-    epoch = 1
+    mlp_config = MLPConfig()
+    # mlp_config = replace(mlp_config, batch_size=128)
 
-    classifier = DigitClassifier(
-        hidden_layer_size, train_batch_size, learning_rate, epoch
-    )
+    mlp = MLP(mlp_config)
+    print(mlp)
+    mlp_trainer = MLPTrainer(mlp_config, mlp)
+    mlp_evaluator = MLPEvaluator(mlp_config, mlp)
+
     start = time.perf_counter()
-    classifier.train()
-    classifier.evaluate()
+    metrics = mlp_trainer.train()
+    accuracy = mlp_evaluator.evaluate()
     end = time.perf_counter()
-    print(f"Accuracy: {classifier.accuracy}%, train + eval time: {end - start:.3f} sec")
-    classifier.plot_metrics()
+    print(f"Accuracy: {accuracy}%, train + eval time: {end - start:.3f} sec")
+
+    mlp_plotter = MLPPlotter(metrics, mlp_config)
+    mlp_plotter.plot_metrics()
 
 
 if __name__ == "__main__":
